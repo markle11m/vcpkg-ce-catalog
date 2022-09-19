7580ec5e7da2d4b5fd171e1413cc9d21f27fe2f0
Mark Levine <markle@microsoft.com>
2022-09-19 13:57:44 -0700

    make msbuild projects standalone and move to MSBuild root directory; add standalone shell environment samples

---
+++ demos2/MSBuild/MySolution/demo-project.cmd
@@ -0,0 +1,77 @@
+@echo off
+@setlocal enabledelayedexpansion
+
+set _action=%1
+set _targetArch=%2
+
+for %%a in (clean build rebuild run reset) do (
+    if "%_action%" == "%%a" goto :check_targetArch
+)
+echo %~n0: invalid action '%_action%'& exit /b 1
+
+:check_targetArch
+if "%_targetArch%" == "" (
+    echo INFO: no Platform specified - using x64
+    set _targetArch=x64
+) 
+set _fValidArch=false
+rem Note: the msbuild project/solution does not currently contain arm or arm64 configurations
+for %%a in (x64 x86) do (
+    set _fValidArch=true
+)
+if /I "%_fValidArch%" == "false" (
+    echo invalid Platform architecture '%_targetArch%' - running x64 instead
+    set _targetArch=x64
+)
+set _archOutputDir=!_targetArch!
+if /I "%_targetArch%" == "x86" set _archOutputDir=Win32
+
+:start
+
+for %%i in (yes y true) do @if /I "%_PAUSE%" == "%%i" set _pauseBeforeCommands=true
+for %%i in (no n false) do @if /I "%_PAUSE%" == "%%i" set _pauseBeforeCommands=false
+if "%_pauseBeforeCommands%" == "" set _pauseBeforeCommands=true
+
+set _msbuildConfig=Release
+set _msbuildTarget=%_action%
+goto :%_action%
+
+:rebuild
+:build
+:clean
+if "%$_MSBuildExe%" == "" (
+    echo ERROR: unable to build - variable $_MSBuildExe not set
+    echo - To install MSBuild, please run the install_vs command to download the Visual Studio installer
+    echo - and install the Desktop C++ workload with only the C++ core desktop features option selected.
+    echo - Then re-run the bootstrap command to update the environment.
+    exit /b 1
+)
+set $_MSBuildArgs=/t:%_msbuildTarget% /p:Configuration=%_msbuildConfig% /p:Platform=%_targetArch% /p:EnableExperimentalVcpkgIntegration=true
+echo Running %_msbuildTarget% [command=msbuild.exe %$_MSBuildArgs%]
+if /I "%_pauseBeforeCommands%" == "true" pause
+"%$_MSBuildExe%" %$_MSBuildArgs%
+goto :done
+
+:run
+set $_exeFile=.\Outputs\%_archOutputDir%\%_msbuildConfig%\ConsoleApplication.exe
+if not exist %$_exeFile% (
+    echo ERROR: unable to run - '%$_exeFile%' does not exist
+    exit /b 1
+)
+echo Running '%$_exeFile%'...
+if /I "%_pauseBeforeCommands%" == "true" pause
+%$_exeFile%
+goto :done
+
+:reset
+set $_outputDir=.\Outputs\%_archOutputDir%
+if not exist %$_outputDir% (
+    echo INFO: no output directory to delete
+    goto :done
+)
+set $cmd=rd /s /q %$_outputDir%
+echo Deleting output directory [%$cmd%]...
+%$cmd%
+
+:done
+exit /b 0
---
+++ demos2/MSBuild/NativeProjectsSolution/ATLProject1/ATLProject1.cpp
@@ -0,0 +1,30 @@
+// ATLProject1.cpp : Implementation of WinMain
+
+
+#include "pch.h"
+#include "framework.h"
+#include "resource.h"
+#include "ATLProject1_i.h"
+
+
+using namespace ATL;
+
+
+class CATLProject1Module : public ATL::CAtlExeModuleT< CATLProject1Module >
+{
+public :
+	DECLARE_LIBID(LIBID_ATLProject1Lib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_ATLPROJECT1, "{0e8da8e2-0007-4aa5-8595-31e5eb1c30b0}")
+};
+
+CATLProject1Module _AtlModule;
+
+
+
+//
+extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
+								LPTSTR /*lpCmdLine*/, int nShowCmd)
+{
+	return _AtlModule.WinMain(nShowCmd);
+}
+
